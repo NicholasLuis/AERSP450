9f35303679fdb7896ad34df496da5238e9981b8e
NicholasLuis <nml5604@psu.edu>
2024-09-15 11:24:22 -0400

    Fixed Problem 1

--- HW1/NL_HW1_Problem1.m
+++ HW1/NL_HW1_Problem1.m
@@ -7,28 +7,28 @@ clc; clear; close all;
 MU = 3.986 * (10^5); % km^3 / s^2
 R_E = 6371; % Radius of earth in km
 f = 0:0.1:360; % Iterates the true anomaly by 0.1
-V0 = sqrt(MU / R_E); % Constant speed value
+V0 = sqrt(MU / (3*R_E)); % Constant speed value
 
 %% Caclculations where m1 = m2
-r1 = sqrt((3*R_E*cosd(f)-0.5*R_E).^2 + (3*R_E*sind(f)).^2);
-r2 = sqrt((3*R_E*cosd(f)+0.5*R_E).^2 + (3*R_E*sind(f)).^2);
+r1 = sqrt((3*R_E*cosd(f)-(0.5*R_E)).^2 + (3*R_E*sind(f)).^2);
+r2 = sqrt((3*R_E*cosd(f)+(0.5*R_E)).^2 + (3*R_E*sind(f)).^2);
 
-a1 = MU*((0.5*V0^2) - (MU./r1)).^2;
-a2 = MU*((0.5*V0^2) - (MU./r2)).^2;
+a1 = -0.5*MU*((0.5*(V0^2)) - (MU./r1)).^(-1);
+a2 = -0.5*MU*((0.5*(V0^2)) - (MU./r2)).^(-1);
 
-e1 = sqrt(1 - (( (r1.^2).*(V0^2)) ./ (MU.*a1)) );
-e2 = sqrt(1 - (( (r2.^2).*(V0^2)) ./ (MU.*a2)) );
+e1 = sqrt(1 - ( (r1.^2) .* (V0^2) ./ (MU.*a1) ));
+e2 = sqrt(1 - ( (r2.^2) .* (V0^2) ./ (MU.*a2) ));
 
-%% Calculations where 2*m1 = m2
+%% Calculations where m1 = 2*m2
 
-r3 = sqrt((3*R_E*cosd(f)-(1/3)*R_E).^2 + (3*R_E*sind(f)).^2);
-r4 = sqrt((3*R_E*cosd(f)+(2/3)*R_E).^2 + (3*R_E*sind(f)).^2);
+r3 = sqrt((3*R_E*cosd(f)-((1/3)*R_E)).^2 + (3*R_E*sind(f)).^2);
+r4 = sqrt((3*R_E*cosd(f)+((2/3)*R_E)).^2 + (3*R_E*sind(f)).^2);
 
-a3 = MU*((0.5*V0^2) - (MU./r3)).^2;
-a4 = MU*((0.5*V0^2) - (MU./r4)).^2;
+a3 = -0.5*MU*((0.5*(V0^2)) - (MU./r3)).^(-1);
+a4 = -0.5*MU*((0.5*(V0^2)) - (MU./r4)).^(-1);
 
-e3 = sqrt(1 - (( (r3.^2).*(V0^2)) ./ (MU.*a3)) );
-e4 = sqrt(1 - (( (r4.^2).*(V0^2)) ./ (MU.*a4)) );
+e3 = sqrt(1 - ( (r3.^2) .* (V0^2) ./ (MU.*a3) ));
+e4 = sqrt(1 - ( (r4.^2) .* (V0^2) ./ (MU.*a4) ));
 
 %% Plots
 
